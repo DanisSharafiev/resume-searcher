9ed6cafe9d14f7727359f718fc1d1efabca0cde3
Danis Sharafiev <harnemer@gmail.com>
2025-06-28 17:04:40 +0300

    feature: init base structure and datasets

---
+++ backend/gateway/config/config.py
@@ -0,0 +1,21 @@
+class PostgresConfig:
+    def __init__(self, host: str, port: int, user: str, password: str, database: str):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.database = database
+
+    def get_connection_string(self) -> str:
+        return f"postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
+
+class QdrantConfig:
+    def __init__(self, host: str, port: int, user: str, password: str, database: str):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.database = database
+
+    def get_connection_string(self) -> str:
+        return f"qd://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
